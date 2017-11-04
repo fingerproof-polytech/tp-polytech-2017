b825fd151b87eccad9ead5e8166929e0960bc652
Sebastien P <sp@fing.pro>
2017-11-04 15:33:12 +0100

    Add search query and results basic logic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { DetailsPage } from '../details/details';
+
+interface Result {
+  author: string;
+  date: number;
+  image: string;
+  title: string;
+}
+
+const fakeResults: Result[] = [{
+  author: 'Author 1',
+  date: 2017,
+  image: 'http://lorempixel.com/300/300/',
+  title: 'Result 1'
+}, {
+  author: 'Author 2',
+  date: 2017,
+  image: 'http://lorempixel.com/300/300/',
+  title: 'Result 2'
+}];
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  detailsPage: typeof DetailsPage = DetailsPage;
+  query: string = '';
+  results: Result[];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor() {
+    this.getResults();
   }
 
+  getResults(): void {
+    switch (this.query.length) {
+    case 0:
+      this.results = [];
+      break;
+    case 1:
+    case 2:
+      break;
+    default:
+      this.results = fakeResults;
+    }
+  }
 }
