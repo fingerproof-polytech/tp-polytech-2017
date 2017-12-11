e9d2dfe03ef2963519f3da3019a6d0014ef044b2
Sebastien P <sp@fing.pro>
2017-12-11 12:15:50 +0100

    Add switchMap operator

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,6 @@
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/pluck';
+import 'rxjs/add/operator/switchMap';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
